d822e4fa4c471160aa86d96059574ba8a1f29e01
Felipe de Oliveira Souza <felipe.oliveirac8@gmail.com>
2024-02-10 09:45:42 -0300

    ci: change vite config to build app on pipeline

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  esbuild: {
+    target: 'esnext',
+    platform: 'linux',
+  },
   plugins: [
     laravel({
-      input: ["resources/src/index.tsx"],
+      input: ['resources/src/index.tsx'],
       refresh: true,
     }),
     react(),
   ],
   resolve: {
     alias: {
-      "@": "/resources/src",
+      '@': '/resources/src',
     },
   },
 });
