b2fc178d4ffe7e7c8b6f25e6e32c98f8316306a8
Felipe de Oliveira Souza <felipe.oliveirac8@gmail.com>
2024-02-24 23:44:21 -0300

    fix: change seo pageview mode

--- resources/src/core/utils/seo.ts
+++ resources/src/core/utils/seo.ts
@@ -1,16 +1,26 @@
-import optionsService from '../services/options/options.service';
+// import optionsService from '../services/options/options.service';
 
 export async function gaPageView(title: string, path: string) {
-  const googleTag = await optionsService.getOption('GOOGLE_TAG_ID');
+  // const googleTag = await optionsService.getOption('GOOGLE_TAG_ID');
 
-  if (
-    googleTag !== false &&
-    'gtag' in window &&
-    typeof window.gtag === 'function'
-  ) {
-    window.gtag('event', 'page_view', {
-      page_title: title,
-      page_location: path,
+  // if (
+  //   googleTag !== false &&
+  //   'gtag' in window &&
+  //   typeof window.gtag === 'function'
+  // ) {
+  //   window.gtag('event', 'page_view', {
+  //     page_title: title,
+  //     page_location: path,
+  //   });
+  // }
+
+  if ('dataLayer' in window && Array.isArray(window.dataLayer)) {
+    window.dataLayer?.push({
+      event: 'pageview',
+      page: {
+        page_title: title,
+        page_location: path,
+      },
     });
   }
 }
--- resources/src/pages/Public/Home/index.tsx
+++ resources/src/pages/Public/Home/index.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
 import { CallToAction, Features, Hero } from '@/components/Home';
+import { gaPageView } from '@/core/utils';
 
 export default function HomePage() {
+  useEffect(() => {
+    const path = window.location.pathname + window.location.search;
+
+    gaPageView('Home', path);
+  }, []);
+
   return (
     <main>
       <Hero />
